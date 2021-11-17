9eb1858bb801bd02a27d3d4164002e478a5209d2
sunghoon72 <chung72>
2021-11-17 00:49:18 -0600

    Add Rest API module

--- client/src/App.js
+++ client/src/App.js
@@ -21,34 +21,26 @@ table: {
 
 })
 
-const customers = [{
-  'id': 1,
-  'image': 'https://placeimg.com/64/64/1',
-  'name': 'john',
-  'birthday': '940201',
-  'gender':  'm',
-  'job': 'lecturer' 
-},
-{
-  'id': 2,
-  'image': 'https://placeimg.com/64/64/2',
-  'name': 'ray',
-  'birthday': '970811',
-  'gender':  'm',
-  'job': 'designer' 
-},
-{
-  'id': 3,
-  'image': 'https://placeimg.com/64/64/3',
-  'name': 'aiden',
-  'birthday': '960514',
-  'gender':  'm',
-  'job': 'teacher' 
-}
-]
+
 
 
 class App extends Component {
+
+  state = {
+    customers: ""
+  };
+
+  componentDidMount(){
+    this.callApi()
+      .then(res => this.setState({customers: res}))
+      .catch(err => console.log(err));
+  }
+
+  callApi = async()=>{
+    const response = await fetch('/api/customers');
+    const body = await response.json();
+    return body;  
+  }
   render(){
     const {classes} = this.props;
   return (
@@ -65,7 +57,7 @@ class App extends Component {
           </TableRow>
         </TableHead>
         <TableBody>
-         {customers.map(c=> {return ( <Customer 
+         {this.state.customers ? this.state.customers.map(c=> {return ( <Customer 
          key= {c.id}
          id = {c.id}
          image = {c.image}
@@ -75,7 +67,7 @@ class App extends Component {
          job = {c.job}
          />  );
          })
-         }
+         : "" }
         </TableBody>
         </Table>
 
--- server.js
+++ server.js
@@ -6,8 +6,31 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.get('/api/hello',(req, res) => {
-    res.send({message: 'Hello Express!'});
+app.get('/api/customers', (req,res) =>{
+    res.send([{
+        'id': 1,
+        'image': 'https://placeimg.com/64/64/1',
+        'name': 'john',
+        'birthday': '940201',
+        'gender':  'm',
+        'job': 'lecturer' 
+      },
+      {
+        'id': 2,
+        'image': 'https://placeimg.com/64/64/2',
+        'name': 'ray',
+        'birthday': '970811',
+        'gender':  'm',
+        'job': 'designer' 
+      },
+      {
+        'id': 3,
+        'image': 'https://placeimg.com/64/64/3',
+        'name': 'aiden',
+        'birthday': '960514',
+        'gender':  'm',
+        'job': 'teacher' 
+      }]);
 });
 app. listen(port, ()=> console.log(`Listening on port ${port}`));
 
